bae75705c38230c84a83f04d2e28eef92bcde97e
Lou <lou.d.chaney@gmail.com>
2019-12-03 12:42:34 -0500

    Updates

---
+++ precache-manifest.dd3f5235c90066adc199aad8d5a7b013.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbd24b5061bfe64d432b6d68b9bec9a",
+    "url": "/lou-chaney-project5/index.html"
+  },
+  {
+    "revision": "acbf938e55d3895efed3",
+    "url": "/lou-chaney-project5/static/css/main.7c864a82.chunk.css"
+  },
+  {
+    "revision": "01384dc7ab6b975d7c96",
+    "url": "/lou-chaney-project5/static/js/2.e9d0bafe.chunk.js"
+  },
+  {
+    "revision": "acbf938e55d3895efed3",
+    "url": "/lou-chaney-project5/static/js/main.c06ce20c.chunk.js"
+  },
+  {
+    "revision": "49533ab57a14242ffa18",
+    "url": "/lou-chaney-project5/static/js/runtime-main.b4f1665a.js"
+  }
+]);
