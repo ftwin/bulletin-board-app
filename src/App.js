22f58dc28dc71aebe12c592538527b4997505d23
Lou Chaney <28720819+ftwin@users.noreply.github.com>
2022-01-24 18:12:07 -0500

    WIP convert components to functional components

--- src/App.js
+++ src/App.js
@@ -1,64 +1,56 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import firebase from './Firebase.js';
-import Notes from './Note.js';
-import NewNote from './NewNote.js';
+// import Notes from './Note.js';
+// import NewNote from './NewNote.js';
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      notesApp: [],
-      notes: [],
-      image: "",
-      alt: ""
-    }
-  }
+const App = () => {
 
+  const [notes, setNotes ] = useState();
+  // const [image, setImage ] = useState("");
+  // const [alt, setAlt ] = useState("");
+
+  
   //connect app to firebase
-  componentDidMount(){
+  useEffect(()=> {
     const dbRef = firebase.database().ref();
-
-    //listen on the dbRef for when the value changes
+    let notesArray;
     dbRef.on('value', (snapshot) =>{
-      const notes = snapshot.val();
-      const notesArray = Object.entries(notes)    
-      this.setState({
-        notes: notesArray
-      })
+      const notesVal = snapshot.val();
+      notesArray = notesVal;
+    console.log(notesArray)
     })
-  }
+    setNotes([notesArray]);
+  },[]);
 
-
-  render(){
-    return (
-      <div className="container">
-        <main className="wrapper">
-          <div className="info">
-            <h1>Bulletin Board</h1>
-            <NewNote newNoteProp={this.addNewNote} />
-          </div>
-          {
-            this.state.notes.reverse().map((note) => {
-              return (
-                <Notes 
-                  noteText={note[1].note} 
-                  key={note[0]} 
-                  noteId={note[0]}
-                  noteImage={note[1].image}
-                  altText={note[1].altText}
-                  savedComments={note[1]} 
-                  newCommentProp={this.addNewComment} 
-                />
-                
-              )
-            })
-          }          
-        </main>
-        <footer><p>Copyright <a href="https://www.louchaney.com">Lou Chaney</a> 2019</p></footer>
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <main className="wrapper">
+        <div className="info">
+          <h1>Bulletin Board</h1>
+          {/* <NewNote newNoteProp={this.addNewNote} /> */}
+        </div>
+        {
+          notes
+          // .reverse().map((note) => {
+          //   return (
+          //     <Notes 
+          //       noteText={note[1].note} 
+          //       key={note[0]} 
+          //       noteId={note[0]}
+          //       noteImage={note[1].image}
+          //       altText={note[1].altText}
+          //       savedComments={note[1]} 
+          //       newCommentProp={this.addNewComment} 
+          //     />
+              
+          //   )
+          // })
+        }          
+      </main>
+      <footer><p>Copyright <a href="https://www.louchaney.com">Lou Chaney</a> 2022</p></footer>
+    </div>
+  );
 }
 
 export default App;
